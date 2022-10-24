6d0fa88c316c8aa6f4281c5ab1a605770a0efa84
kkum <kirpalanik@national.ffbatiment.fr>
2022-10-24 09:45:26 +0200

    keepasscommander: update to 3.1

--- bucket/keepasscommand-git-credential.ps1
+++ bucket/keepasscommand-git-credential.ps1
@@ -1,6 +1,6 @@
-Write-Host "Updating git Credential Helper for user"
 $kpPath = Split-Path $(scoop which keepass.exe) -Parent -Resolve
 $gcPath = Join-Path $kpPath -ChildPath "Plugins/git-credential-keepasscommand.exe"
+Write-Host "Updating git Credential Helper for user $gcPath"
 
 git config --global --unset credential.helper
 git config --global credential.helper $gcPath

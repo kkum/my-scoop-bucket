3d9698697aa885dc097ad657c9133983b12bd660
kkum <kirpalanik@national.ffbatiment.fr>
2023-01-10 16:09:23 +0100

    fix keepasscommand git script

--- bucket/keepasscommand-git-credential.ps1
+++ bucket/keepasscommand-git-credential.ps1
@@ -1,6 +1,5 @@
 $kpPath = Split-Path $(scoop which keepass.exe) -Parent -Resolve
-$gcPath = Join-Path $kpPath -ChildPath "Plugins/git-credential-keepasscommand.exe"
+$gcPath = (join-Path $kpPath -ChildPath "Plugins/git-credential-keepasscommand.exe") -replace '\\','/' 
 Write-Host "Updating git Credential Helper for user $gcPath"
-
-git config --global --unset credential.helper
-git config --global credential.helper $gcPath
+    git config --unset credential.helper
+    git config credential.helper "$gcPath"
